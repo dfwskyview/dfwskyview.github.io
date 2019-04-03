594cab7be42cb53006a70a5b24a4525c2b40d618
Dan Rose <dan.rose@wnco.com>
2019-04-03 16:11:56 -0500

    Replace Kuula with marzipano

---
+++ vendor/data.js
@@ -0,0 +1,160 @@
+var APP_DATA = {
+  "scenes": [
+/*
+    {
+      "id": "0-state-fair-dallas-tx",
+      "name": "State Fair, Dallas TX",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.944344865200117,
+        "pitch": 0.5504129438654513,
+        "fov": 1.568238662786406
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.719564633572416,
+          "pitch": -0.059329878941547065,
+          "rotation": 3.141592653589793,
+          "target": "2-prosper-tx"
+        }
+      ],
+      "infoHotspots": []
+    },
+*/
+    {
+      "id": "2-prosper-tx",
+      "name": "Prosper TX",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.12711418657676532,
+        "pitch": 1.0313230743550967,
+        "fov": 0.8205978547407539
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-graff-farms-irving-tx",
+      "name": "Graff Farms, Irving TX",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -2.9859347281095765,
+        "pitch": 0.3731365796466726,
+        "fov": 0.7518004748344423
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-mbbc--irving-tx",
+      "name": "MBBC,  Irving TX",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.8644929102216423,
+        "pitch": 0.6977247431703493,
+        "fov": 0.7518004748344423
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+  ],
+  "name": "Panoramas",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
